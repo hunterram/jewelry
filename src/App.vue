a8159e249d018a0c8c0bc0f13d42201bd06bdea6
krish Thapaliya <ramthapaliya8888@gmail.com>
2023-03-26 13:32:56 +0545

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <header>
+    <ul class="left-nav">
+      <li>Home</li>
+      <li>Products</li>
+    </ul>
+
+    <img class="logo" src="" />
+
+    <ul class="right-nav">
+      <li>Services</li>
+      <li>Contacts</li>
+    </ul>
+  </header>
+
+</template>
+
+<style>
+
+  body {
+    margin: 0px;
+    padding: 0px;
+  }
+
+  header {
+    background-color: #111111;
+    height: 70px;
+    display: flex;
+  }
+
+  ul.left-nav {}
+  ul.left-nav li {}
+
+  img.logo {}
+
+  ul.right-nav {}
+  ul.right-nav li { }
+
+
+</style>
