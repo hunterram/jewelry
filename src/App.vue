c454d54cd5c623e1bffdc52466b50184615032dc
krish Thapaliya <ramthapaliya8888@gmail.com>
2023-04-05 13:36:52 +0545

    exclusive products

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
 
 <template>
 
-<header>
+    <!-- Header and Navigation  -->
+    <header>
         <div class="wrapper wrap">
             <ul class="left-nav">
                 <li>Home</li>
@@ -12,7 +13,7 @@
             </ul>
     
             <div class="logo">
-                <img src="./assets/logo.svg" alt="Jewelry Logo" title="Jewelry Logo" />
+                <img src="./images/logo.svg" alt="Jewelry Logo" title="Jewelry Logo" />
             </div>
     
             <ul class="right-nav">
@@ -22,10 +23,11 @@
         </div>
     </header>
 
+    <!-- Hero Section -->
     <section class="hero">
         <div class="wrapper wrap">
             <div class="image">
-                <img src="./assets/banner-image.jpg" alt="Jewelry">
+                <img src="./images/banner-image.jpg" alt="Jewelry">
             </div>
 
             <div class="content">
@@ -37,6 +39,84 @@
             </div>
 
         </div>
+    </section>
+
+    <!-- Exclusive Products -->
+    <section class="exclusive">
+        <div class="wrapper wrap">
+
+            <div class="total-products">
+                <h3>3000</h3>
+                <h4>exclusive products</h4>
+            </div>
+
+            <ul class="categories">
+                <li>
+                    <div class="image">
+                        <img src="./images/rings.svg" />
+                    </div>
+                    <p>Rings</p>
+                </li>
+
+                <li>
+                    <div class="image">
+                        <img src="./images/bracelets.png" />
+                    </div>
+                    <p>Bracelets</p>
+                </li>
+
+                <li>
+                    <div class="image">
+                        <img src="./images/necklace.png" />
+                    </div>
+                    <p>Necklace</p>
+                </li>
+
+                <li>
+                    <div class="image">
+                        <img src="./images/earrings.png" />
+                    </div>
+                    <p>Earrings</p>
+                </li>
+
+                <li>
+                    <div class="image">
+                        <img src="./images/tiaras.png" />
+                    </div>
+                    <p>Tiaras</p>
+                </li>
+
+                <li>
+                    <div class="image">
+                        <img src="./images/watches.png" />
+                    </div>
+                    <p>Watchers</p>
+                </li>
+            </ul>
+
+            <div class="offers">
+                <div class="collection">
+                    <div class="content">
+                        <h2>New</h2>
+                        <h2>Spring</h2>
+                        <h2>Collection</h2>
+                    </div>
+                </div>
+                <div class="off">
+                    <div class="content">
+                        <h3>20% Off</h3>
+                        <p>In publishing and graphic design, Lorem ipsum is a placeholder</p>
+                        <a href="#">Products</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- Products store -->
+    <section class="products">
+        <div class="wrapper"></div>
     </section>
 
 </template>
